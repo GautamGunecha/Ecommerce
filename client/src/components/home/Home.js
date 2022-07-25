12298c291d2f98603f52cf9f2d98f87e06919cf7
Gautam Gunecha <gautamgunecha@gmail.com>
2022-07-25 12:08:46 +0530

    Header and Navbar.

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import Header from './header/Header'
+import { useLocation } from 'react-router'
+
+const HomePage = () =>
+{
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
+};
 
 const Home = () =>
 {
+    const location = useLocation();
+
+    const Page = () =>
+    {
+        if (location.pathname === "/") return <HomePage />;
+    };
     return (
-        <div>Home</div>
-    )
-}
+        <React.Fragment>
+            <Page />
+        </React.Fragment>
+    );
+};
 
 export default Home
---
+++ client/src/components/home/header/Header.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react'
+import "./Header.css";
+import { BiCart, BiUser } from "react-icons/bi";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FiLogOut } from "react-icons/fi";
+import { Link, useNavigate } from "react-router-dom";
+import headerData from '../../data/Header.json'
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from '../../../redux/actions/auth/authAction'
+
+const Header = () =>
+{
+    const dispatch = useDispatch();
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+    const navigation = useNavigate();
+
+    const logoutHandler = () =>
+    {
+        dispatch(logout());
+        navigation("/");
+    };
+
+    useEffect(() => { }, [userInfo]);
+
+    return (
+        <React.Fragment>
+            <div className="header">
+                <div className="headerLogo">
+                    <a href={`http://localhost:3000`}>
+                        <p>{headerData.Brand.Title}</p>
+                    </a>
+                </div>
+                <div className="headerNav">
+                    {headerData.Navbar.map((navLinks) => (
+                        <a
+                            key={navLinks.id}
+                            href={`http://localhost:3000/${navLinks.link}`}
+                        >
+                            <p>{navLinks.title}</p>
+                        </a>
+                    ))}
+                </div>
+                <div className="headerIcons">
+                    {userInfo && (
+                        <Link to={"/profile"}>
+                            <img
+                                src={userInfo.avatar}
+                                alt={userInfo.name}
+                                className="headerIcon"
+                            />
+                        </Link>
+                    )}
+
+                    {userInfo && (
+                        <Link to={"/wishlist"}>
+                            <AiOutlineHeart size={20} className="headerIcon heartIcon" />
+                        </Link>
+                    )}
+
+                    {!userInfo && (
+                        <Link to={"/sign-in"}>
+                            <BiUser size={20} className="headerIcon" />
+                        </Link>
+                    )}
+
+                    <Link to={"/mantra-shoping-cart"}>
+                        <BiCart size={20} className="headerIcon shopingCart" />
+                    </Link>
+                    {userInfo && (
+                        <div>
+                            <FiLogOut
+                                onClick={logoutHandler}
+                                size={20}
+                                className="headerIcon"
+                            />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Header
