b3920601c673110a1923a229d30c360bc9260912
Gautam Gunecha <gautamgunecha@gmail.com>
2022-07-27 16:02:30 +0530

    .env url

--- client/src/redux/utils/url.js
+++ client/src/redux/utils/url.js
@@ -1,2 +1,1 @@
-// export const url = process.env.REACT_APP_URL || "https://mantracom.herokuapp.com"
-export const url = "https://mantracom.herokuapp.com"
+export const url = process.env.REACT_APP_URL 
